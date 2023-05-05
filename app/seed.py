7f7a317273457cbc10cd5814d57255660c526b08
Julia Lee <jlee30@stuy.edu>
2023-05-05 12:49:59 -0400

    Create transportation table

--- app/seed.py
+++ app/seed.py
@@ -1,6 +1,6 @@
 import sqlite3
-database_file = "nycInfo"
-connection = sqlite3.conect(database_file, check_same_thread=False)
+database_file = "nycInfo.db"
+connection = sqlite3.connect(database_file, check_same_thread=False)
 
 #create transportation table
 connection.execute("CREATE TABLE IF NOT EXISTS" +
@@ -18,6 +18,12 @@ connection.execute("CREATE TABLE IF NOT EXISTS" +
                 "cong_dist INTEGER, " +
                 "type TEXT, " +
                 "owner TEXT, " +
+                "FEMAFldz TEXT, " +
+                "FEMAFldT TEXT, " +
+                "HrcEvac INTEGER, " +
+                "latitude REAL, " +
+                "longitude REAL, " +
+                "the_geom TEXT"
                 ")")
 print("transportation table created")
 connection.commit()
