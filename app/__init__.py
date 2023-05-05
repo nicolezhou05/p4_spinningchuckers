0c1bb12ebbc4501d403cad287c18cce66a15def4
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2023-05-05 13:13:02 -0400

    .

--- app/__init__.py
+++ app/__init__.py
@@ -4,32 +4,32 @@ app = Flask(__name__)
 # home page
 @app.route("/")
 def root():
-    return render_template('home.html')
+    return render_template('index.html')
 
-# crime rates page
-@app.route("/crimerates")
-def crimepage():
-    return render_template('crime.html')
+# # crime rates page
+# @app.route("/crimerates")
+# def crimepage():
+#     return render_template('crime.html')
 
-# environment tracker page (weather/climate)
-@app.route("/environment")
-def environment():
-    return render_template('env.html')
+# # environment tracker page (weather/climate)
+# @app.route("/environment")
+# def environment():
+#     return render_template('env.html')
 
-# transportation page
-@app.route("/transportation")
-def transportion():
-    return render_template('trans.html')
+# # transportation page
+# @app.route("/transportation")
+# def transportion():
+#     return render_template('trans.html')
 
-# energy page
-@app.route("/energy")
-def energy():
-    return render_template('energy.html')
+# # energy page
+# @app.route("/energy")
+# def energy():
+#     return render_template('energy.html')
 
-# job market page
-@app.route("/job")
-def crimepage():
-    return render_template('job.html')
+# # job market page
+# @app.route("/job")
+# def crimepage():
+#     return render_template('job.html')
 
 if __name__ == "__main__":
     app.debug = True
