882d8e8121488c6b83f08239b0ecb718d82d2ae9
nicole <nzhou30@stuy.edu>
2023-05-05 13:07:13 -0400

    starting py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
+# home page
 @app.route("/")
 def root():
     return render_template('home.html')
 
+# crime rates page
+@app.route("/crimerates")
+def crimepage():
+    return render_template('crime.html')
+
+# environment tracker page (weather/climate)
+@app.route("/environment")
+def environment():
+    return render_template('env.html')
+
+# transportation page
+@app.route("/transportation")
+def transportion():
+    return render_template('trans.html')
+
+# energy page
+@app.route("/energy")
+def energy():
+    return render_template('energy.html')
+
+# job market page
+@app.route("/job")
+def crimepage():
+    return render_template('job.html')
 
 if __name__ == "__main__":
     app.debug = True
