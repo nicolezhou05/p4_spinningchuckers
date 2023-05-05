d9312e34e84d0522fea77b33897d08c108c75e57
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2023-05-05 13:05:31 -0400

    nav

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+// Get the navbar
+var navbar = document.querySelector('.navbar');
+
+// Get the links inside the navbar
+var links = navbar.querySelectorAll('a');
+
+// Loop through each link and add a click event listener
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function() {
+    // Remove the active class from all links
+    for (var j = 0; j < links.length; j++) {
+      links[j].classList.remove('active');
+    }
+    // Add the active class to the clicked link
+    this.classList.add('active');
+  });
+}
