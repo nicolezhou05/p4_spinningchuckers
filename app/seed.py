bae88f0d95972537da8aea1439a376653ec55e79
Julia Lee <jlee30@stuy.edu>
2023-05-08 13:12:44 -0400

    Working on filling table values

--- app/seed.py
+++ app/seed.py
@@ -2,40 +2,50 @@ import sqlite3
 database_file = "nycInfo.db"
 connection = sqlite3.connect(database_file, check_same_thread=False)
 
-# #create transportation table
-# connection.execute("CREATE TABLE IF NOT EXISTS" +
-#                 " transportation (" +
-#                 "VMS INTEGER, " +
-#                 "main_roadway TEXT, " +
-#                 "direction TEXT, " +
-#                 "cross_street TEXT, " +
-#                 "borough TEXT, " +
-#                 "barcode INTEGER, " +
-#                 "borocd INTEGER, " +
-#                 "coun_dist INTEGER, " +
-#                 "assem_dist INTEGER, " +
-#                 "st_sen_dist INTEGER, " +
-#                 "cong_dist INTEGER, " +
-#                 "type TEXT, " +
-#                 "owner TEXT, " +
-#                 "FEMAFldz TEXT, " +
-#                 "FEMAFldT TEXT, " +
-#                 "HrcEvac INTEGER, " +
-#                 "latitude REAL, " +
-#                 "longitude REAL, " +
-#                 "the_geom TEXT"
-#                 ")")
-# print("transportation table created")
-# connection.commit()
-def create(cursor):
-    query = "INSERT INTO transportation VALUES("
-    for r in dataList:
-        query += 
+#create transportation table
+connection.execute("CREATE TABLE IF NOT EXISTS" +
+                " transportation (" +
+                "VMS INTEGER, " +
+                "main_roadway TEXT, " +
+                "direction TEXT, " +
+                "cross_street TEXT, " +
+                "borough TEXT, " +
+                "barcode INTEGER, " +
+                "borocd INTEGER, " +
+                "coun_dist INTEGER, " +
+                "assem_dist INTEGER, " +
+                "st_sen_dist INTEGER, " +
+                "cong_dist INTEGER, " +
+                "type TEXT, " +
+                "owner TEXT, " +
+                "FEMAFldz TEXT, " +
+                "FEMAFldT TEXT, " +
+                "HrcEvac INTEGER, " +
+                "latitude REAL, " +
+                "longitude REAL, " +
+                "the_geom TEXT"
+                ")")
+print("transportation table created")
+connection.commit()
+
+def create(cursor, data):
+    query = "INSERT INTO transportation (VMS, main_roadway, direction, cross_street, borough, barcode, borocd, coun_dist, assem_dist, st_sen_dist, cong_dist, type, owner, FEMAFldz, FEMAFldT, HrcEvac, latitude, longitude, the_geom) VALUES ("
+    TEXT_indeces = [1, 2, 3, 4, 11, 12, 13, 14, 18]
+    for r in range(len(data)):
+        for c in range(len(data[r])):
+            if c in TEXT_indeces:
+                query += "'" + data[r][c] + "',"
+            else:
+                query += data[r][c] + ", "
+        print("This is row " + str(r))
+    query += ")"
+    print(query)
+    cursor.execute(query)
+    cursor.connection.commit()
 
 with open("Overhead_Electronic_Signs.csv", "r") as file:
     dataList = file.read().split("\n")
     for r in range(len(dataList)):
         dataList[r] = dataList[r].split(",")
-    print(dataList)
 
-    headings = dataList[0]
+    create(cursor=connection.cursor(), data=dataList[1:])
