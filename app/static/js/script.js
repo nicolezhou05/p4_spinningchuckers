fb2ef0578df695269d8393f65b666c9c8cac5979
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2023-05-08 13:07:51 -0400

    .

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,7 +3,10 @@
 var navbar = document.querySelector('.navbar');
 
 // Get the links inside the navbar
-var links = navbar.querySelectorAll('a');
+var links = navbar.querySelectorAll('.navtab');
+
+// Gets an array of all the tabs
+var tabs = document.querySelectorAll(".tab");
 
 // Loop through each link and add a click event listener
 for (var i = 0; i < links.length; i++) {
@@ -14,5 +17,18 @@ for (var i = 0; i < links.length; i++) {
     }
     // Add the active class to the clicked link
     this.classList.add('active');
+
+    // Changes the current tab showing
+    for (var c = 0; c < tabs.length; c++) {
+      var current = tabs[c];
+      // Loop through each tab and check if they have the class selected
+      // Compares the classname with the innerHTML of the navbar tab
+      if (current.id == `${this.innerHTML.toLowerCase()}` && current.classList.contains('hidden')) {
+        current.classList.remove('hidden');
+      }
+      else {
+        current.classList.add('hidden');
+      }
+    }
   });
 }
