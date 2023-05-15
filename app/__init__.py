05ca36bea0783e4805a744b3f6589ab5d376d621
nicole <nzhou30@stuy.edu>
2023-05-15 13:21:32 -0400

    register

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,21 @@ def root():
 #
 # @app.route("/register")
 # def register():
-#     # work in progress
+#     username = request.form['register_username']
+#     password = request.form['register_pswd']
+#     same_pswd = request.form['pswd_confirm']
+#     # if the two entered passwords match, add the user info to the database
+#     if confirm(password, same_pswd):
+#         # if account if created, user directed to the home page
+#         if create_user(username, password):
+#             session["username"] = username
+#             return redirect("/home")
+#         # if not, returns a message to tell the user to retry
+#         else:
+#             return render_template('register.html', message="Account creation failed. Please try again.")
+#     # if not, user stays on the register page
+#     return render_template('register.html',message="Passwords do not match.")
+
 #
 # @app.route("/logout", methods = ["POST"])
 # def logout():
