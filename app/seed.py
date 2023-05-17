dddc1f3b28c2576300897757674d83c33fb847ea
Nicholas Tarsis <ntarsis30@stuy.edu>
2023-05-17 12:26:45 -0400

    changed issues with query

--- app/seed.py
+++ app/seed.py
@@ -26,7 +26,7 @@ connection.execute("CREATE TABLE IF NOT EXISTS" +
                 "the_geom TEXT"
                 ")")
 print("transportation table created")
-connection.commit()
+#connection.commit()
 
 def create_transportation(cursor, data):
     query = "INSERT INTO transportation (VMS, main_roadway, direction, cross_street, borough, barcode, borocd, coun_dist, assem_dist, st_sen_dist, cong_dist, type, owner, FEMAFldz, FEMAFldT, HrcEvac, latitude, longitude, the_geom) VALUES "
@@ -45,44 +45,42 @@ def create_transportation(cursor, data):
     print(query)
     cursor.execute(query)
     cursor.connection.commit()
-
+"""
 with open("Overhead_Electronic_Signs.csv", "r") as file:
     dataList = file.read().split("\n")
     for r in range(len(dataList)):
         dataList[r] = dataList[r].split(",")
 
     create_transportation(cursor=connection.cursor(), data=dataList[1:])
-
+"""
 #create crime table
 connection.execute("CREATE TABLE IF NOT EXISTS" +
                 " crime (" +
-                "Date TEXT, " +
-                "Title TEXT, " +
-                "Organization TEXT, " +
-                "cross_street TEXT, " +
-                "City TEXT, " +
-                "State TEXT, " +
-                "URL TEXT, " +
-                "Keyword TEXT, " +
-                "Summary TEXT" +
+                "Title VARCHAR, " +
+                "Organization VARCHAR, " +
+                "cross_street VARCHAR, " +
+                "City VARCHAR, " +
+                "State VARCHAR, " +
+                #"URL VARCHAR, " +
+                "Keyword VARCHAR, " +
+                "Summary VARCHAR" +
                 ")")
 print("crime table created")
 connection.commit()
 def create_crime(cursor, data):
-    query = "INSERT INTO crime (Date, Title, Organization, cross_street, City, State, URL, Keyword, Summary) VALUES "
-    TEXT_indices = list(range(1,9))
+    query = "INSERT INTO crime (Title, Organization, cross_street, City, State, Keyword, Summary) VALUES "
+    TEXT_indices = [1,2,3,4,5,7,8]
     for r in range(len(data)-1):
         query += "("
         for c in range(len(data[r])):
             if len(data[r][c]) < 1:
-                query += "NULL" + ", "
+                query += "NULL, "
             elif c in TEXT_indices:
-                query += "'" + data[r][c] + "', "
-            else:
-                query += data[r][c] + ", "
+                query += str(data[r][c]) + ", "
+            #else:
+                #query += str(data[r][c]) + ", "
         query = query[:len(query)-2] + "),"
     query = query[:len(query)-2] + ");"
-    print(query)
     cursor.execute(query)
     cursor.connection.commit()
 
@@ -183,42 +181,3 @@ with open("US_Crime_Data.csv", "r") as file:
 
 
 
-#create crime table
-connection.execute("CREATE TABLE IF NOT EXISTS" +
-                " crime (" +
-                "Date TEXT, " +
-                "Title TEXT, " +
-                "Organization TEXT, " +
-                "cross_street TEXT, " +
-                "City TEXT, " +
-                "State TEXT, " +
-                "URL TEXT, " +
-                "Keyword TEXT, " +
-                "Summary TEXT" +
-                ")")
-print("crime table created")
-connection.commit()
-def create_crime(cursor, data):
-    query = "INSERT INTO crime (Date, Title, Organization, cross_street, City, State, URL, Keyword, Summary) VALUES "
-    TEXT_indices = list(range(1,9))
-    for r in range(len(data)-1):
-        query += "("
-        for c in range(len(data[r])):
-            if len(data[r][c]) < 1:
-                query += "NULL" + ", "
-            elif c in TEXT_indices:
-                query += "'" + data[r][c] + "', "
-            else:
-                query += data[r][c] + ", "
-        query = query[:len(query)-2] + "),"
-    query = query[:len(query)-2] + ");"
-    print(query)
-    cursor.execute(query)
-    cursor.connection.commit()
-
-with open("US_Crime_Data.csv", "r") as file:
-    dataList = file.read().split("\n")
-    for r in range(len(dataList)):
-        dataList[r] = dataList[r].split(",")
-
-    create_crime(cursor=connection.cursor(), data=dataList[1:])
