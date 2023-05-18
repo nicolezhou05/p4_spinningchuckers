349cd8d752fc55fe56e6acdd09e9a8c922da994c
julia <jlee30@stuy.edu>
2023-05-17 23:40:44 -0400

    trying to make table

--- app/jobs.py
+++ app/jobs.py
@@ -4,19 +4,40 @@ database_file = "nycInfo.db"
 connection = sqlite3.connect(database_file, check_same_thread=False)
 #create energy table
 connection.execute("CREATE TABLE IF NOT EXISTS" +
-                " energy (" +
-                "Property_ID INTEGER, " +
-                "Property_Name TEXT, " +
-                "Parent_Property_ID TEXT, " +
-                "Parent_Property_Name TEXT, " +
-                "Month TEXT, " +
-                "Electricity_Use TEXT, " +
-                "Natural_Gas_Use TEXT" +
+                " jobs (" +
+                "job_id INTEGER, " +
+                "agency TEXT, " +
+                "posting_type TEXT, " +
+                "num_of_positions INTEGER, " +
+                "business_title TEXT, " +
+                "civic_service_title TEXT, " +
+                "title_code_no TEXT, " +
+                "level TEXT, " +
+                "job_category TEXT, " +
+                "full_part_time_indicator TEXT, " +
+                "salary_range_from INTEGER, " +
+                "salary_range_to INTEGER, " +
+                "salary_freq TEXT, " +
+                "work_location TEXT, " +
+                "division_work_unit TEXT, " +
+                "job_description TEXT, " +
+                "minimum_qual_reqs TEXT, " +
+                "pref_skills TEXT, " +
+                "additional_info TEXT, " +
+                "to_apply TEXT, " +
+                "hours_shift TEXT, " +
+                "work_location1 TEXT, " +
+                "recruitment_contact TEXT, " +
+                "residency_req TEXT, " +
+                "posting_date TEXT, " +
+                "post_until TEXT, " +
+                "posting_updated TEXT, " +
+                "process_date TEXT"
                 ")")
-print("energy table created")
+print("jobs table created")
 connection.commit()
-def create_energy(cursor, data):
-    query = "INSERT INTO energy (Property_ID, Property_Name, Parent_Property_ID, Parent_Property_Name, Month, Electricity_Use, Natural_Gas_Use) VALUES "
+def create_jobs(cursor, data):
+    query = "INSERT INTO jobs (agency, posting_type, num_of_positions, business_title, civic_service_title, title_code_no, level, job_category, full_part_time_indicator, salary_range_from, salary_range_to, salary_freq, work_location, division_work_unit, job_description, minimum_qual_reqs, pref_skills, additional_info, to_apply, hours_shift, work_location1, recruitment_contact, residency_req, posting_date, post_until, posting_updated, process_date) VALUES "
     TEXT_indices = list(range(1,8))
     for r in range(len(data)-1):
         query += "("
@@ -34,9 +55,9 @@ def create_energy(cursor, data):
     cursor.connection.commit()
 
 
-with open("Local_Law_84_2021__Monthly_Data_for_Calendar_Year_2020_.csv", "r") as file:
+with open("nyc-jobs.csv", "r", encoding="utf-8") as file:
     dataList = file.read().split("\n")
     for r in range(len(dataList)):
         dataList[r] = dataList[r].split(",")
 
-    create_energy(cursor=connection.cursor(), data=dataList[1:])
+    create_jobs(cursor=connection.cursor(), data=dataList[1:])
