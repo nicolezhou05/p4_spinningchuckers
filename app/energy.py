03737b40658933ec0c6d513db698d3a37834fd24
Nicholas Tarsis <ntarsis30@stuy.edu>
2023-05-19 12:55:09 -0400

    got energy table in sql

--- app/energy.py
+++ app/energy.py
@@ -1,11 +1,11 @@
 import sqlite3
 import csv
-database_file = "nycInfo.db"
+database_file = "nycInfo3.db"
 connection = sqlite3.connect(database_file, check_same_thread=False)
 #create energy table
 connection.execute("CREATE TABLE IF NOT EXISTS" +
                 " energy (" +
-                "Property_ID INTEGER, " +
+                "Property_ID TEXT, " +
                 "Property_Name TEXT, " +
                 "Parent_Property_ID TEXT, " +
                 "Parent_Property_Name TEXT, " +
@@ -17,16 +17,17 @@ print("energy table created")
 connection.commit()
 def create_energy(cursor, data):
     query = "INSERT INTO energy (Property_ID, Property_Name, Parent_Property_ID, Parent_Property_Name, Month, Electricity_Use, Natural_Gas_Use) VALUES "
-    TEXT_indices = list(range(1,8))
-    for r in range(len(data)-1):
+    TEXT_indices = list(range(7))
+    for r in range(400):
         query += "("
-        for c in range(len(data[r])):
+        for c in range(7):
             if len(data[r][c]) < 1:
                 query += "NULL" + ", "
             elif c in TEXT_indices:
                 query += "'" + data[r][c] + "', "
             else:
-                query += data[r][c] + ", "
+                break
+                #query += data[r][c] + ", "
         query = query[:len(query)-2] + "),"
     query = query[:len(query)-2] + ");"
     print(query)
