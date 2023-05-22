f5007962dda4ec3a9965803d133d4b4bc282b831
nicole <nzhou30@stuy.edu>
2023-05-22 12:44:44 -0400

    move for now

--- app/__init__.py
+++ app/__init__.py
@@ -3,48 +3,48 @@ from login import user_exist, create_user, confirm, get_pswd, correct_login
 app = Flask(__name__)
 
 # home page
-@app.route("/home")
+@app.route("/")
 def root():
     return render_template('index.html')
 
-@app.route("/", methods = ["POST","GET"])
-def log():
-    if request.method == "GET":
-        return render_template("login.html")
-    elif request.method == "POST":
-        username = request.form['username']
-        password = request.form['pswd']
-        # checks if using is registered
-        if (user_exist(username)):
-            # checks if the entered username and password are correct
-            if correct_login(username, password):
-                session["username"] = username
-                return redirect("/home")
-            else:
-                return render_template("login.html", message="Incorrect Password")
-        else:
-            return render_template("login.html", message="User not found")
+# @app.route("/", methods = ["POST","GET"])
+# def log():
+#     if request.method == "GET":
+#         return render_template("login.html")
+#     elif request.method == "POST":
+#         username = request.form['username']
+#         password = request.form['pswd']
+#         # checks if using is registered
+#         if (user_exist(username)):
+#             # checks if the entered username and password are correct
+#             if correct_login(username, password):
+#                 session["username"] = username
+#                 return redirect("/home")
+#             else:
+#                 return render_template("login.html", message="Incorrect Password")
+#         else:
+#             return render_template("login.html", message="User not found")
 
-@app.route("/register", methods = ["POST","GET"])
-def register():
-    if request.method == "GET":
-        return render_template("register.html")
-    elif request.method == "POST":
-        username = request.form['register_username']
-        password = request.form['register_pswd']
-        same_pswd = request.form['pswd_confirm']
-        # if the two entered passwords match, add the user info to the database
-        if confirm(password, same_pswd):
-            # if account if created, user directed to the home page
-            if create_user(username, password):
-                session["username"] = username
-                return redirect("/home")
-            # if not, returns a message to tell the user to retry
-            else:
-                return render_template('register.html', message="Account creation failed. Please try again.")
-        # if not, user stays on the register page
-        else:
-            return render_template('register.html',message="Passwords do not match.")
+# @app.route("/register", methods = ["POST","GET"])
+# def register():
+#     if request.method == "GET":
+#         return render_template("register.html")
+#     elif request.method == "POST":
+#         username = request.form['register_username']
+#         password = request.form['register_pswd']
+#         same_pswd = request.form['pswd_confirm']
+#         # if the two entered passwords match, add the user info to the database
+#         if confirm(password, same_pswd):
+#             # if account if created, user directed to the home page
+#             if create_user(username, password):
+#                 session["username"] = username
+#                 return redirect("/home")
+#             # if not, returns a message to tell the user to retry
+#             else:
+#                 return render_template('register.html', message="Account creation failed. Please try again.")
+#         # if not, user stays on the register page
+#         else:
+#             return render_template('register.html',message="Passwords do not match.")
 
 
 @app.route("/logout", methods = ["POST"])
